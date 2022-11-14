a4edf5fdf1c211f599afdf48618fce36b59fba27
fuzhou.chen <fuzhouch@gmail.com>
2022-11-14 17:11:03 +0800

    [NEW][UTFail] Demo basic logic for smoke test.

--- query_test.go
+++ query_test.go
@@ -1,17 +1,34 @@
 package query
 
 import (
+	"bufio"
 	"testing"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/fuzhouch/query/format/csv"
 	"github.com/fuzhouch/query/media/fs"
 )
 
 func TestQuery_SmokeTest(t *testing.T) {
 	mem := afero.NewMemMapFs()
-	fs, err := fs.New(fs.WithAferoFs(mem))
+	media, err := fs.New(fs.WithAferoFs(mem))
 	assert.Nil(t, err)
-	assert.NotNil(t, fs)
+	assert.NotNil(t, media)
+
+	format, err := csv.New("test.csv")
+	assert.Nil(t, err)
+
+	// Semantics:
+	// SELECT * FROM test_csv_file LIMITS 1;
+	queryExpr := Select(AllFields()).Top(1)
+	stmt, err := queryExpr.From(mem)
+	assert.Nil(t, err)
+
+	scanner := bufio.NewScanner(stmt.Reader())
+	for scanner.Next() {
+		text := scanner.Text()
+		assert.NotEqual(t, "", text)
+	}
 }
